94b9ab302a98e67e35122fca27d11b634fb4f5e6
juanmurillo <juanmurilloquinto@gmail.com>
2026-05-01 01:16:00 -0500

    Algoritmo simple con javascript seamana 3 dia 1

--- decision-comunitaria.js
+++ decision-comunitaria.js
@@ -1,6 +1,6 @@
 const propuesta = "Mejorar la comunicación del barrio"
-let tieneLenguajeClaro = true
-let pideDatosNecesarios = true
+let tieneLenguajeClaro = vardadero
+let pideDatosNecesarios = verdadero
 
 console.log("Revisión de propuesta comunitaria")
 console.log("Propuesta:", propuesta)
